286ef97acb7b0d176477bc5ee298a06ff0b5d58e
pamsn <pauloadmsn@gmail.com>
2021-12-02 20:56:34 -0300

    adicionando atributo marca

--- produto.java
+++ produto.java
@@ -3,12 +3,16 @@ public class Produto {
 	private String nome;
 	private double preco;
 	private int quantidade;
+	private String marca;
 
-	public Produto(String nome, double preco, int quantidade){
+	public Produto(String nome, double preco, int quantidade, String marca){
 		this.nome = nome;
 		this.preco = preco;
 		this.quantidade = quantidade;
+		this.marca = marca;
 	}
-
-	
 }
+
+
+
+
