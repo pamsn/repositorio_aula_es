50f35d659c2511b7841b4eca3c7a88d9415fb6c4
pamsn <pauloadmsn@gmail.com>
2022-01-20 12:42:11 -0300

    renomeando atributo tamanho

--- produto.java
+++ produto.java
@@ -4,14 +4,14 @@ public class Produto {
 	private double preco;
 	private int quantidade;
 	private String marca;
-	private String tamanho;
+	private String tam;
 
-	public Produto(String nome, double preco, int quantidade, String marca, String tamanho){
+	public Produto(String nome, double preco, int quantidade, String marca, String tam){
 		this.nome = nome;
 		this.preco = preco;
 		this.quantidade = quantidade;
 		this.marca = marca;
-		this.tamanho = tamanho;
+		this.tam = tam;
 	}
 }
 
